ac766aa12978380e823ffe601854f1d56adb3125
andreluizsilv <109443784+andreluizsilv@users.noreply.github.com>
2022-11-30 23:56:24 -0300

    finalizando

--- filme/views.py
+++ filme/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render, redirect, reverse
-from .models import Filme
-#from .forms import CriarContaForm, FormHomepage
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomepage
 from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = "homepage.html"
+    form_class = FormHomepage
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('filme:homefilmes')
+        else:
+            return super().get(request, *args, **kwargs)
 
 
-class Homefilmes(ListView):
+    def get_success_url(self):
+        email = self.request.POST.get("email")
+        usuarios = Usuario.objects.filter(email=email)
+        if usuarios:
+            return reverse('filme:login')
+        else:
+            return reverse('filme:criarconta')
+
+# object_list -> lista de itens do modelo
+class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
     model = Filme
-    #object_list
+# object -> 1 item do nosso modelo
 
-
-class Detalhefilmes(DetailView):
+class Detalhefilmes(LoginRequiredMixin, DetailView):
     template_name = "detalhesfilmes.html"
     model = Filme
 
@@ -33,7 +48,8 @@ class Detalhefilmes(DetailView):
         context["filmes_relacionados"] = filmes_relacionados
         return context
 
-class PesquisaFilmes(ListView):
+
+class PesquisaFilme(LoginRequiredMixin, ListView):
     template_name = "pesquisa.html"
     model = Filme
 
@@ -45,12 +61,32 @@ class PesquisaFilmes(ListView):
         else:
             return None
 
+class Paginaperfil(LoginRequiredMixin, UpdateView):
+    template_name = "editarperfil.html"
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
+
+
+class Criarconta(FormView):
+    template_name = "criarconta.html"
+    form_class = CriarContaForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('filme:login')
+
 #def homepage(request):
-#    return render(request, "homepage.html")
+    #return render(request, "homepage.html")
 
-
+# url - view - html
 #def homefilmes(request):
-#     context = {}
-#     lista_filmes = Filme.objects.all()
-#     context['lista_filmes'] = lista_filmes
+#    context = {}
+#    lista_filmes = Filme.objects.all()
+#    context['lista_filmes'] = lista_filmes
 #    return render(request, "homefilmes.html", context)
